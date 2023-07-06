1bd22fd14fd080d75791fe7b714a332d60bda48e
Kalyanimalokar <82643974+Kalyanimalokar@users.noreply.github.com>
2023-07-06 21:44:10 +0530

    Update showPageMap.js

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = pk.eyJ1Ijoia2FseWFuaW1hbG9rYXIiLCJhIjoiY2xqZzZzcGlhMDh5czNncDk5M3NpYWZmaCJ9.OIhbhFPYEUIQnYsc4S3kVA
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FseWFuaW1hbG9rYXIiLCJhIjoiY2xqZzZzcGlhMDh5czNncDk5M3NpYWZmaCJ9.OIhbhFPYEUIQnYsc4S3kVA'
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
